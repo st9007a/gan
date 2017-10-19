43188cd416e98b774a1f0e2786fc206789225d67
uno <st9007a@gmail.com>
2017-10-20 00:20:00 +0800

    Fix bug in ConditionalGAN

--- data/util.py
+++ data/util.py
@@ -1,6 +1,15 @@
 import numpy as np
 
+from random import randint
+
 def get_noise_sample(batch_size, samples):
     return np.random.uniform(-1., 1., size = [batch_size, samples])
+
+def get_random_condition(batch_size):
+    cond = []
+    for _ in range(batch_size):
+        cond.append([1 if randint(0, 9) == i else 0 for i in range(10)])
+
+    return np.array(cond)
 
 
--- run.py
+++ run.py
@@ -1,32 +1,36 @@
 import numpy as np
 
-from nn.gan import GAN
+from nn.gan import GAN, ConditionalGAN
 from data.dataset import mnist
-from data.util import get_noise_sample
+from data.util import get_noise_sample, get_random_condition
 
-batch_size = 256
+batch_size = 100
+sample_size = 100
 
 dataset = mnist()
 
-model = GAN()
+model = ConditionalGAN()
 model.build()
 
 for step in range(1, 100001):
     for _ in range(1):
-        noise = get_noise_sample(batch_size, 128)
+        noise = get_noise_sample(batch_size, sample_size)
+        fake_condition = get_random_condition(batch_size)
 
-        fake_data = model.generator().predict(noise)
-        real_data, _ = dataset.get_batch(batch_size)
+        fake_data = model.generator().predict([noise, fake_condition])
+        real_data, real_condition = dataset.get_batch(batch_size)
 
-        x = np.concatenate((real_data, fake_data))
+        x_data = np.concatenate((real_data, fake_data))
+        x_condition = np.concatenate((real_condition, fake_condition))
         y = np.ones([2 * batch_size, 1])
         y[batch_size:, :] = 0
 
-        d_loss = model.discriminator().train_on_batch(x, y)
+        d_loss = model.discriminator().train_on_batch([x_data, x_condition], y)
 
-    noise = get_noise_sample(batch_size, 128)
+    noise = get_noise_sample(batch_size, sample_size)
+    condition = get_random_condition(batch_size)
     y = np.ones([batch_size, 1])
-    g_loss = model.gan().train_on_batch(noise, y)
+    g_loss = model.gan().train_on_batch([noise, condition], y)
 
     if step % 100 == 0:
         print("step " + str(step) + " d_loss: " + str(d_loss) + " g_loss: " + str(g_loss))
